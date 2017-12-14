fc594af3d224884bbd36965f7346de65004d426e
Ix5231 <ix5231@gmail.com>
2017-12-14 19:16:18 +0900

    関数化

--- src/main.ts
+++ src/main.ts
@@ -4,13 +4,32 @@ import * as io from 'socket.io-client'
 
 namespace spock {
     const KEYCODE = {
-        a: 65,
-        two: 98, four: 100,
-        six: 102,
-        eight: 104,
+       a: 65,
+       two: 98,
+       four: 100,
+       six: 102,
+       eight: 104,
     };
 
     let game = new Phaser.Game(800, 600, Phaser.AUTO, '', { preload: preload, create: create, update: update });
+
+    let player: Phaser.Sprite;
+    let player2: Phaser.Sprite;
+
+    let score_player = 0;
+    let score_player2 = 0;
+
+    // group
+    let platforms;
+    let stars;
+    let diamonds;
+
+    let scoreText;
+    let scoreText2;
+    let setText;
+
+    let cursors;
+    let a, two, four, six, eight;
 
     function preload() {
         game.load.image('sky', 'assets/sky.png');
@@ -21,79 +40,24 @@ namespace spock {
         game.load.image('diamond', 'assets/diamond.png');
     }
 
-    let player: Phaser.Sprite;
-    let player2: Phaser.Sprite;
-
-    let score_player;
-    let score_player2;
-
-    // group
-    let platforms;
-    let cursors;
-    let a, two, four, six, eight;
-
-    let stars;
-    let diamonds;
-    let scoreText;
-    let scoreText2;
-    let setText;
-
     function create() {
         // 物理
         game.physics.startSystem(Phaser.Physics.ARCADE);
-
-
         game.add.sprite(0, 0, 'sky');
 
-        // 足場グループ
-        platforms = game.add.group();
-        platforms.enableBody = true;
+        setup_field();
+        setup_players();
+        setup_stars();
 
-        // 足場生成
-        let ground = platforms.create(0, game.world.height - 64, 'ground');
-        ground.scale.setTo(2, 2);
-        ground.body.immovable = true;
+        // 一定時間ごとに星作成
+        game.time.events.repeat(Phaser.Timer.SECOND * 2, 10, () =>  {
+            var stars = game.add.group();
+            stars.enableBody = true;
+        }, this);
 
-        let ledge = platforms.create(400, 400, 'ground');
-        ledge.body.immovable = true;
-        ledge = platforms.create(-150, 250, 'ground');
-        ledge.body.immovable = true;
+        setup_ui();
+        setup_input();
 
-        // プレイヤー作成
-        player = game.add.sprite(32, game.world.height - 150, 'dude');
-        player2 = game.add.sprite(700, game.world.height - 150, 'baddie');
-
-        player.animations.add('left', [0, 1, 2, 3], 10, true);
-        player.animations.add('right', [5, 6, 7, 8], 10, true);
-        player2.animations.add('left', [0, 1], 10, true);
-        player2.animations.add('right', [2, 3], 10, true);
-
-        // 星追加
-        stars = game.add.group();
-        stars.enableBody = true;
-        diamonds = game.add.group();
-        diamonds.enableBody = true;
-
-        for (var i = 0; i < 12; i++) {
-            var star = stars.create(i * 70, 0, 'star');
-            star.body.gravity.y = 300;
-            star.body.bounce.y = 0.7 + Math.random() * 0.2;
-        }
-
-        game.time.events.repeat(Phaser.Timer.SECOND * 2, 10, createstar, this);
-
-        // スコア
-        scoreText = game.add.text(16, 16, 'P1.score: 0', { fontSize: 32, fill: '#000' });
-        scoreText2 = game.add.text(600, 16, 'P2.score: 0', { fontSize: 32, fill: '#000' });
-        setText = game.add.text(300, 16, 'Counter: 0', { fontSize: 32, fill: '#000' });
-        
-        // 操作
-        cursors = game.input.keyboard.createCursorKeys();
-        a = game.input.keyboard.addKey(KEYCODE.a);
-        two = game.input.keyboard.addKey(KEYCODE.two);
-        four = game.input.keyboard.addKey(KEYCODE.four);
-        six = game.input.keyboard.addKey(KEYCODE.six);
-        eight = game.input.keyboard.addKey(KEYCODE.eight);
         // タイマ
         game.time.events.loop(Phaser.Timer.SECOND, updateCounter, this);
     }
@@ -104,9 +68,18 @@ namespace spock {
         game.physics.arcade.collide(player2, platforms);
         game.physics.arcade.collide(stars, platforms);
 
-        //game.physics.arcade.overlap(player, stars, collectStar, undefined, this);
-        game.physics.arcade.overlap(player, stars, collectStar, undefined, this);
-        game.physics.arcade.overlap(player2, stars, collectStar2, undefined, this);
+        game.physics.arcade.overlap(player, stars, (_, star) => {
+            star.kill();
+
+            score_player += 10;
+            scoreText.text = 'P1.score: ' + score_player;
+        }, undefined, this);
+        game.physics.arcade.overlap(player2, stars, (_, star) => {
+            star.kill();
+
+            score_player2 += 10;
+            scoreText.text = 'P2.score: ' + score_player2;
+        }, undefined, this);
 
         // 移動
         player.body.velocity.x = 0;
@@ -143,7 +116,7 @@ namespace spock {
         {
             //if(i == 0)
             //{
-            var diamond = diamonds.create(player.x, player.y, 'diamond');
+            let diamond = diamonds.create(player.x, player.y, 'diamond');
             diamond.body.velocity.x = 300;
             //i = 1;
             //}
@@ -175,6 +148,75 @@ namespace spock {
         }
     }
 
+    // プレイヤー追加
+    function setup_players() {
+        function setup_player_sprite(p: Phaser.Sprite) {
+            game.physics.arcade.enable(p);
+
+            p.body.bounce.y = 0
+                p.body.gravity.y = 300
+                p.body.collideWorldBounds = true
+
+                p.animations.add('left', [0, 1, 2, 3], 10, true);
+            p.animations.add('right', [5, 6, 7, 8], 10, true);
+        } 
+
+        player = game.add.sprite(32, game.world.height - 150, 'dude');
+        player2 = game.add.sprite(700, game.world.height - 150, 'baddie');
+
+        setup_player_sprite(player);
+        setup_player_sprite(player2);
+    }
+
+    // 星追加
+    function setup_stars() {
+        stars = game.add.group();
+        stars.enableBody = true;
+        diamonds = game.add.group();
+        diamonds.enableBody = true;
+
+        for (let i = 0; i < 12; i++) {
+            let star = stars.create(i * 70, 0, 'star');
+            star.body.gravity.y = 300;
+            star.body.bounce.y = 0.7 + Math.random() * 0.2;
+        }
+    }
+
+    // フィールド作成
+    function setup_field() {
+        // 足場グループ
+        platforms = game.add.group();
+        platforms.enableBody = true;
+
+        // 足場生成
+        let ground = platforms.create(0, game.world.height - 64, 'ground');
+        ground.scale.setTo(2, 2);
+        ground.body.immovable = true;
+
+        let ledge = platforms.create(400, 400, 'ground');
+        ledge.body.immovable = true;
+        ledge = platforms.create(-150, 250, 'ground');
+        ledge.body.immovable = true;
+    }
+
+    // UIセットアップ
+    function setup_ui() {
+        // スコア
+        scoreText = game.add.text(16, 16, 'P1.score: 0', { fontSize: 32, fill: '#000' });
+        scoreText2 = game.add.text(600, 16, 'P2.score: 0', { fontSize: 32, fill: '#000' });
+        setText = game.add.text(300, 16, 'Counter: 0', { fontSize: 32, fill: '#000' });
+    }
+
+    // 操作セットアップ
+    function setup_input() {
+        cursors = game.input.keyboard.createCursorKeys();
+        a = game.input.keyboard.addKey(KEYCODE.a);
+        two = game.input.keyboard.addKey(KEYCODE.two);
+        four = game.input.keyboard.addKey(KEYCODE.four);
+        six = game.input.keyboard.addKey(KEYCODE.six);
+        eight = game.input.keyboard.addKey(KEYCODE.eight);
+    }
+
     function collectStar(_, star) {
         star.kill();
 
@@ -189,19 +231,14 @@ namespace spock {
         scoreText2.text = 'P2.score: ' + score_player2;
     }
 
-    function createstar(){
-        var stars = game.add.group();
-        stars.enableBody = true;
-    }
-
     function updateCounter(){
         /*var counter ;
-        counter++;
-        setText.text('Counter: ' + counter);*/
+          counter++;
+          setText.text('Counter: ' + counter);*/
     }
 
-    let socket = io.connect()
-    socket.emit('matching')
+    let socket = io.connect();
+    socket.emit('matching');
 
-    socket.on('playing', () => console.log('Start'))
+    socket.on('playing', () => console.log('Start'));
 }
